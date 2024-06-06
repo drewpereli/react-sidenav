e88392bbf2f29b846904c044bc40323e23226f74
Drew Pereli <drewpereli@gmail.com>
2024-06-06 09:53:53 -0700

    Transition?

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,16 +9,18 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
       <NavBar />
-      <div className="grow overflow-hidden flex">
-        {drawerOpen && (
-          <nav className="h-full p-4 bg-gray-100">
-            <div>asdflkj asdflkj</div>
-            <div>asdflkj asdflkj</div>
-            <div>asdflkj asdflkj</div>
-            <div>asdflkj asdflkj</div>
-            <div>asdflkj asdflkj</div>
-          </nav>
-        )}
+      <div
+        className={`grow overflow-hidden transition-all grid ${
+          drawerOpen ? 'grid-cols-[0_1fr]' : 'grid-cols-[10rem_1fr]'
+        }`}
+      >
+        <nav className={`h-full overflow-hidden py-4 bg-gray-100`}>
+          <div>asdflkj asdflkj</div>
+          <div>asdflkj asdflkj</div>
+          <div>asdflkj asdflkj</div>
+          <div>asdflkj asdflkj</div>
+          <div>asdflkj asdflkj</div>
+        </nav>
         <main className="h-full grow p-8 overflow-y-auto">{children}</main>
       </div>
       <Footer />
